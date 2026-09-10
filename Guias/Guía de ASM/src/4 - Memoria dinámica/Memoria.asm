a2745aab0894307da00ffe98932542a8d4a57a22
Marilin <gomezmarilin614@gmail.com>
2026-09-09 23:41:14 -0300

    completo ejercicio strCmp

--- Guias/Guía de ASM/src/4 - Memoria dinámica/Memoria.asm
+++ Guias/Guía de ASM/src/4 - Memoria dinámica/Memoria.asm
@@ -15,8 +15,39 @@ global strLen
 ; ** String **
 
 ; int32_t strCmp(char* a, char* b)
+; a[RDI]
+; b[RSI]
 strCmp:
-	ret
+	;prólogo 
+	push RBP
+	mov RBP, RSP
+
+.ciclo:
+	movzx EDX, byte [RDI] ; Move with zero-extend: mueve 1 byte y lo extiende con ceros hasta 32 bits.
+	movzx ECX, byte [RSI]
+
+    cmp EDX, ECX
+    jne .distintos        ; Jump if not Equal
+
+    test EDX, EDX         ; ¿es '\0'?
+    je .iguales           ; Jump if Equal: si ambos '\0' → iguales
+
+    inc rdi
+    inc rsi
+    jmp .ciclo
+
+.iguales:
+    xor eax, eax          ; return 0
+    jmp .fin
+
+.distintos:
+    mov eax, 1            ; return 1
+    jmp .fin
+
+.fin:
+	;epílogo
+    pop rbp
+    ret
 
 ; char* strClone(char* a)
 strClone:
