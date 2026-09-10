a5c7d4f0ceddd4d796323c2096fa93ef7eb984c1
Marilin <gomezmarilin614@gmail.com>
2026-09-10 15:34:22 -0300

    completo ejercicios strCmp, strClone y strLen

--- Guias/Guía de ASM/src/4 - Memoria dinámica/Memoria.asm
+++ Guias/Guía de ASM/src/4 - Memoria dinámica/Memoria.asm
@@ -27,7 +27,8 @@ strCmp:
 	movzx ECX, byte [RSI]
 
     cmp EDX, ECX
-    jne .distintos        ; Jump if not Equal
+    jl .menor             ; Jump if less
+    jg .mayor             ; Jump if greater
 
     test EDX, EDX         ; ¿es '\0'?
     je .iguales           ; Jump if Equal: si ambos '\0' → iguales
@@ -40,8 +41,12 @@ strCmp:
     xor eax, eax          ; return 0
     jmp .fin
 
-.distintos:
+.menor:
     mov eax, 1            ; return 1
+    jmp .fin
+
+.mayor:
+    mov eax, -1            ; return -1
     jmp .fin
 
 .fin:
@@ -50,8 +55,34 @@ strCmp:
     ret
 
 ; char* strClone(char* a)
+; a[RDI]
 strClone:
-	ret
+	;prólogo 
+	push RBP
+	mov RBP, RSP
+
+    mov RSI, RDI         ; guardamos puntero original en rsi
+    call strLen          ; devuelve la longitud del string en eax
+    inc EAX              ; +1 para '\0'
+    mov EDI, EAX         ; Por convención de llamadas, malloc espera el tamaño en RDI
+    call malloc          ; devuelve puntero en RAX
+
+.copiar:
+    mov AL, [RSI]        ; leer char
+    mov [RAX], AL        ; escribir char
+
+    test AL, AL          ; ¿es '\0'?
+    je .fin              ; Jump if Equal: si '\0' → terminamos de copiar el string
+
+    inc RSI
+    inc RAX
+    jmp .copiar
+
+.fin:
+    ;puntero destino ya está en RAX
+	;epílogo
+    pop RBP
+    ret
 
 ; void strDelete(char* a)
 strDelete:
@@ -62,7 +93,27 @@ strPrint:
 	ret
 
 ; uint32_t strLen(char* a)
+; a[RDI]
 strLen:
-	ret
+	;prólogo 
+	push RBP
+	mov RBP, RSP
+
+    xor EAX, EAX         ; longitud = 0
+.ciclo:
+	movzx EDX, byte [RDI]
+
+    test EDX, EDX        ; ¿es '\0'?
+    je .fin              ; Jump if Equal: si '\0' → terminamos de recorrer el string
+
+    inc EAX              ; incremento la longitud en 1
+    inc RDI
+    jmp .ciclo
+
+.fin:
+    ; la longitud ya está guardada en eax
+    ;epílogo
+    pop RBP
+    ret
 
 
