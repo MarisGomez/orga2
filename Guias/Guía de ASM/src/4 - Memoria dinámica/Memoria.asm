3f25d50d441a8801d000df2ca9aa64869ac7481b
Marilin <gomezmarilin614@gmail.com>
2026-09-10 21:45:09 -0300

    termino Memoria.asm

--- Guias/Guía de ASM/src/4 - Memoria dinámica/Memoria.asm
+++ Guias/Guía de ASM/src/4 - Memoria dinámica/Memoria.asm
@@ -3,6 +3,8 @@ extern free
 extern fprintf
 
 section .data
+msg_null: db 'NULL', 10
+format_string: db '%s', 10
 
 section .text
 
@@ -14,6 +16,7 @@ global strLen
 
 ; ** String **
 
+; ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ; int32_t strCmp(char* a, char* b)
 ; a[RDI]
 ; b[RSI]
@@ -54,44 +57,86 @@ strCmp:
     pop rbp
     ret
 
+; ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ; char* strClone(char* a)
 ; a[RDI]
 strClone:
 	;prólogo 
 	push RBP
 	mov RBP, RSP
+    push RBX
+    sub RSP, 8           ; muevo el tope de la pila 8 bytes para que quede alineada
 
-    mov RSI, RDI         ; guardamos puntero original en rsi
+    mov RBX, RDI         ; guardamos puntero original en rbx (registro no volátil)
     call strLen          ; devuelve la longitud del string en eax
     inc EAX              ; +1 para '\0'
     mov EDI, EAX         ; Por convención de llamadas, malloc espera el tamaño en RDI
     call malloc          ; devuelve puntero en RAX
 
+    mov RDX, RAX         ; copio RAX en RDI para no incrementar RAX
 .copiar:
-    mov AL, [RSI]        ; leer char
-    mov [RAX], AL        ; escribir char
+    mov CL, [RBX]        ; leer char (no podemos usar AL ni BL pq corrompemos los punteros fuente (RBX ) y destino (RAX))
+    mov [RDX], CL        ; escribir char
 
-    test AL, AL          ; ¿es '\0'?
+    test CL, CL          ; ¿es '\0'?
     je .fin              ; Jump if Equal: si '\0' → terminamos de copiar el string
 
-    inc RSI
-    inc RAX
+    inc RBX
+    inc RDX
     jmp .copiar
 
 .fin:
     ;puntero destino ya está en RAX
 	;epílogo
+    add RSP, 8
+    pop RBX
     pop RBP
     ret
 
+; ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ; void strDelete(char* a)
 strDelete:
-	ret
+    ;prólogo 
+	push RBP
+	mov RBP, RSP
+    call free
+    ;epílogo
+    pop RBP
+    ret
 
+; ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ; void strPrint(char* a, FILE* pFile)
+; a[RDI]
+; pFIle[RSI]
 strPrint:
-	ret
+    ;prólogo mov AL, [RSI]
+	push RBP
+	mov RBP, RSP
 
+    ; Me fijo si el string es vacío
+    mov AL, [RDI]
+    test AL, AL             ; ¿es NULL?
+    jne .escribir           ; Jump if not equal
+
+    ; Si es NULL, lo escribirmos 
+    mov RDI, RSI            ; primer argumento: FILE*
+    mov RSI, format_string  ; segundo argumento: "%s"
+    mov RDX, msg_null       ; Tercer argumento: "NULL\n"
+    call fprintf            ; fprintf(pFile, "%s", "NULL")
+    jmp .fin
+
+.escribir:
+    mov RDX, RDI            ; Tercer argumento: a (antes de pisar RDI)
+    mov RDI, RSI            ; primer argumento: FILE*
+    mov RSI, format_string  ; segundo argumento: "%s"
+    call fprintf           ; fprintf(pFile, "%s", a)
+
+.fin:
+    ;epílogo
+    pop RBP
+    ret
+
+; ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ; uint32_t strLen(char* a)
 ; a[RDI]
 strLen:
@@ -115,5 +160,3 @@ strLen:
     ;epílogo
     pop RBP
     ret
-
-
