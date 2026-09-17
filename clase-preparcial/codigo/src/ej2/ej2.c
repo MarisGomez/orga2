3610005b9c58101579cc69e164881de42ea5ea99
Marilin <gomezmarilin614@gmail.com>
2026-09-17 15:55:20 -0300

    ejercicios 2 y 3 en c

--- clase-preparcial/codigo/src/ej2/ej2.c
+++ clase-preparcial/codigo/src/ej2/ej2.c
@@ -1,5 +1,50 @@
 #include "../ejs.h"
 
+// Función auxiliar 
+void eliminar_publicaciones_del_usuario(feed_t* feed, uint32_t id_usuario) {
+
+  publicacion_t* actual = feed->first;
+  publicacion_t* previa = NULL;
+  bool encontramos_nuevo_first = false;
+
+  while (actual != NULL) {
+    publicacion_t* siguiente = actual->next;
+
+    // vemos si la publicación es del usuario bloqueado o bloqueador
+    bool es_del_usuario = (actual->value->id_autor == id_usuario);
+
+    if (es_del_usuario) {
+      // Eliminamos la publicación del arreglo
+      if (previa != NULL) { // Desenlazamos la publicación actual
+        previa->next = siguiente; // Pasamos de [previa -> actual -> siguiente] a [previa -> siguiente]
+      }
+      free(actual); 
+      
+    } else {
+      // No hacemos nada, no tenemos que borrar la publicación
+      if (!encontramos_nuevo_first) { // (no encontramos nuevo first)
+        feed->first = actual;
+        encontramos_nuevo_first = true;
+      }
+      previa = actual;
+    }
+    actual = siguiente; // avanzamos en el feed
+  }
+
+  if (!encontramos_nuevo_first) { // Suponiendo que todo el feed queda bloqueado o está vacío
+    feed->first = NULL;
+  }
+}
+
 void bloquearUsuario(usuario_t *usuario, usuario_t *usuarioABloquear){
-  return;
+  
+  // Agregamos usuarioABloquear al final del arreglo bloqueados de usuario
+  usuario->bloqueados[usuario->cantBloqueados] = usuarioABloquear;
+  usuario->cantBloqueados++;
+
+  // Eliminamos las publicaciones de usuarioABloquear del feed de usuario
+  eliminar_publicaciones_del_usuario(usuario->feed, usuarioABloquear->id);
+
+  // Eliminamos las publicaciones de usuario del feed de usuarioABloquear
+  eliminar_publicaciones_del_usuario(usuarioABloquear->feed, usuario->id);
 }
--- clase-preparcial/codigo/src/ej3/ej3.c
+++ clase-preparcial/codigo/src/ej3/ej3.c
@@ -1,6 +1,44 @@
 #include "../ejs.h"
 
+// Función auxiliar: Cuenta la cantidad de tuits sobresalientes del feed de un usuario
+uint32_t cuantos_tuits_trending_topic(feed_t* feed, uint32_t user_id, 
+                                      uint8_t (*esTuitSobresaliente)(tuit_t *)) {
+    
+    uint32_t contador = 0;
+    publicacion_t* actual = feed->first;
+    while (actual != NULL) { // Recorremos el feed
+        tuit_t * tuit = actual->value;
+        if (esTuitSobresaliente(tuit) && tuit->id_autor == user_id) {
+            contador++; 
+        }
+        actual = actual->next; 
+    }
+    return contador;
+}
+
+
 tuit_t **trendingTopic(usuario_t *user,
                        uint8_t (*esTuitSobresaliente)(tuit_t *)) {
-    return NULL;
+    
+    // Cantidad de tuits tt del usuario
+    uint32_t cantidad_tuits = cuantos_tuits_trending_topic(user->feed, user->id, esTuitSobresaliente);
+
+    // Si cantidad_tuits es 0, devolvemos NULL
+    if (cantidad_tuits == 0) return NULL;
+
+    // Si cantidad_tuits es distinto a 0, pedimos memoria (+1 para NULL)
+    tuit_t** tuits = (tuit_t**) malloc((cantidad_tuits + 1) * sizeof(tuit_t*));
+
+    uint32_t i = 0;
+    publicacion_t* actual = user->feed->first;
+    while (actual != NULL) { // Comenzamos a recorrer el feed del usuario
+        tuit_t* tuit = actual->value;
+        if (esTuitSobresaliente(tuit) && tuit->id_autor == user->id) {
+            tuits[i] = tuit;
+            i++;
+        }
+        actual = actual->next;
+    }
+    tuits[i] = NULL;
+    return tuits;
 }
