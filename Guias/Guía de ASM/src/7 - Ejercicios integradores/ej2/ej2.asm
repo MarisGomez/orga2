c2ade2147254383e45f24b70f4f10dc13e1d6a1a
Mari <gomezmarilin614@gmail.com>
2026-09-21 16:08:54 -0300

    intento ej2a en asm

--- Guias/Guía de ASM/src/7 - Ejercicios integradores/ej2/ej2.asm
+++ Guias/Guía de ASM/src/7 - Ejercicios integradores/ej2/ej2.asm
@@ -1,4 +1,5 @@
 extern malloc
+extern free
 
 section .rodata
 ; Acá se pueden poner todas las máscaras y datos que necesiten para el ejercicio
@@ -14,7 +15,7 @@ TRUE  EQU 1
 ; Funciones a implementar:
 ;   - optimizar
 global EJERCICIO_2A_HECHO
-EJERCICIO_2A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
@@ -37,15 +38,80 @@ ATTACKUNIT_COMBUSTIBLE EQU 12 ; padding de 1 byte
 ATTACKUNIT_REFERENCES EQU 14
 ATTACKUNIT_SIZE EQU 16 ; padding de 1 byte
 
+; mapa                    [RDI]
+; compartida              [RSI]
+; fun_hash(attackunit_t*) [RDX]
 global optimizar
 optimizar:
-	; Te recomendamos llenar una tablita acá con cada parámetro y su
-	; ubicación según la convención de llamada. Prestá atención a qué
-	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
-	;
-	; r/m64 = mapa_t           mapa
-	; r/m64 = attackunit_t*    compartida
-	; r/m64 = uint32_t*        fun_hash(attackunit_t*)
+	push rbp
+	mov rbp, rsp
+	push r12
+	push r13
+	push r14
+	push r15
+	push rbx
+	sub rsp, 8
+	
+	mov r12, rdi ; mapa[r12]
+	mov r13, rsi ; compartida[r13]
+	mov r14, rdx ; fun_hash[r14]
+
+	xor r15, r15 ; i = 0
+	xor rbx, rbx ; j = 0
+
+	.ciclo_filas:
+	cmp r15, 255
+	je .fin
+		.ciclo_columnas:
+		cmp rbx, 255
+		je .ciclo_filas
+
+		mov r9, [r12 + r15 * 8] ; r9 = mapa[i]
+		mov r8, [r9 + rbx * 8] ; r8 = mapa[i][j]
+		; PRIMER IF:
+		test r8, r8   ;; Caso unidad == NULL
+		je .continue  ;; ||
+		cmp r8, r13   ;; Caso unidad == compartida
+		je .continue  ;;
+		; SEGUNDO IF:
+		mov rdi, r8 ; fun_hash espera a la unidad en rdi
+		call r14
+		mov rdx, rax ; fun_hash(unidad)[RDX]
+
+		mov rdi, r13 ; fun_hash espera a compartida en rdi
+		call r14
+		mov rcx, rax ; fun_hash(compartida)[RCX]
+
+		cmp rdx, rcx
+		jne .continue
+
+		dec byte [r8 + ATTACKUNIT_REFERENCES] ; unidad->references --
+		; TERCER IF:
+		cmp byte [r8 + ATTACKUNIT_REFERENCES], 0 
+		jne .notfree
+
+		.free:
+		mov rdi, r8
+		call free
+
+		.notfree:
+		mov r8, r13
+		inc byte [r13 + ATTACKUNIT_REFERENCES]
+
+		.continue:
+		inc rbx
+		jmp .ciclo_columnas
+	
+	inc r15
+	jmp .ciclo_filas
+
+	.fin:
+	add rsp, 8
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbp
 	ret
 
 global contarCombustibleAsignado
