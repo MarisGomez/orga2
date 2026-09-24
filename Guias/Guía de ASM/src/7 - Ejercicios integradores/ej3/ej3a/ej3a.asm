d1ff5f7f9937fce12da2cf0985d393c65e9fa360
Marilin <gomezmarilin614@gmail.com>
2026-09-23 23:06:49 -0300

    completo ej3a en c y avanzo en asm

--- Guias/Guía de ASM/src/7 - Ejercicios integradores/ej3/ej3a/ej3a.asm
+++ Guias/Guía de ASM/src/7 - Ejercicios integradores/ej3/ej3a/ej3a.asm
@@ -6,29 +6,186 @@ section .text
 
 ; Completar las definiciones (serán revisadas por ABI enforcer):
 USUARIO_ID_OFFSET EQU 0
-USUARIO_NIVEL_OFFSET EQU 0
-USUARIO_SIZE EQU 0
+USUARIO_NIVEL_OFFSET EQU 4
+USUARIO_SIZE EQU 8
 
-CASO_CATEGORIA_OFFSET EQU 0
-CASO_ESTADO_OFFSET EQU 0
-CASO_USUARIO_OFFSET EQU 0
-CASO_SIZE EQU 0
+CASO_CATEGORIA_OFFSET EQU 0 ; padding de 1 byte
+CASO_ESTADO_OFFSET EQU 4    ; padding de 2 bytes
+CASO_USUARIO_OFFSET EQU 8
+CASO_SIZE EQU 16
 
 SEGMENTACION_CASOS0_OFFSET EQU 0
-SEGMENTACION_CASOS1_OFFSET EQU 0
-SEGMENTACION_CASOS2_OFFSET EQU 0
-SEGMENTACION_SIZE EQU 0
+SEGMENTACION_CASOS1_OFFSET EQU 8
+SEGMENTACION_CASOS2_OFFSET EQU 16
+SEGMENTACION_SIZE EQU 24
 
 ESTADISTICAS_CLT_OFFSET EQU 0
-ESTADISTICAS_RBO_OFFSET EQU 0
-ESTADISTICAS_KSC_OFFSET EQU 0
-ESTADISTICAS_KDT_OFFSET EQU 0
-ESTADISTICAS_ESTADO0_OFFSET EQU 0
-ESTADISTICAS_ESTADO1_OFFSET EQU 0
-ESTADISTICAS_ESTADO2_OFFSET EQU 0
-ESTADISTICAS_SIZE EQU 0
+ESTADISTICAS_RBO_OFFSET EQU 1
+ESTADISTICAS_KSC_OFFSET EQU 2
+ESTADISTICAS_KDT_OFFSET EQU 3
+ESTADISTICAS_ESTADO0_OFFSET EQU 4
+ESTADISTICAS_ESTADO1_OFFSET EQU 5
+ESTADISTICAS_ESTADO2_OFFSET EQU 6
+ESTADISTICAS_SIZE EQU 7
+
+;int contar_casos_por_nivel(caso_t* arreglo_casos, int largo, int nivel)
+; arreglo_casos(caso_t*) [RDI]
+; largo                  [RSI]
+; nivel                  [RDX]
+contar_casos_por_nivel:
+push rbp
+mov rbp, rsp
+push r12
+push r13
+push r14
+push r15
+push rbx
+sub rsp, 8
+
+mov r12, rdi ; arreglo_casos[r12]
+mov r13, rsi ; largo[r13]
+mov r14, rdx ; nivel[r14]
+
+xor r15, r15 ; i = 0
+xor rbx, rbx ; contador = 0
+
+.ciclo:
+    cmp r15, r13
+    jae .fin
+
+    mov r8, [r12 + r15 * CASO_SIZE + CASO_USUARIO_OFFSET] ; r8 = caso.usuario
+
+    ; if (caso.usuario->nivel == nivel)
+    mov ecx, dword [r8 + USUARIO_NIVEL_OFFSET] ; exc = caso.usuario->nivel
+
+    cmp ecx, r14d
+    jne .ciclo ; si no son el mismo nivel seguimos iterando
+
+    inc rbx ; contador += 1
+    jmp .ciclo
+
+.fin:
+mov rax, rbx
+
+add rsp, 8
+pop rbx
+pop r15
+pop r14
+pop r13
+pop r12
+pop rbp
+ret
+
 
 ;segmentacion_t* segmentar_casos(caso_t* arreglo_casos, int largo)
+; arreglo_casos(caso_t*) [RDI]
+; largo                  [RSI]
 global segmentar_casos
 segmentar_casos:
+push rbp
+push r12
+push r13
+push r14
+push r15
+push rbx
+sub rsp, 8
+
+mov r12, rdi ; arreglo_casos[r12]
+mov r13d, esi ; largo[r13d]
+
+; INICIALIZO LOS CONTADORES
+; contador0
+mov rdi, r12
+mov rsi, r13
+xor rdx, rdx ; rdx = 0
+call contar_casos_por_nivel
+movzx r14, eax ; contador0
+
+; contador1
+mov rdi, r12
+mov rsi, r13
+mov rdx, 1 ; rdx = 1
+call contar_casos_por_nivel
+movzx r15, eax ; contador1
+
+; contador2
+mov rdi, r12
+mov rsi, r13
+mov rdx, 2 ; rdx = 2
+call contar_casos_por_nivel
+movzx rbx, eax ; contador2
+
+; defino segmento
+mov rdi, SEGMENTACION_SIZE
+call malloc
+mov rbp, rax ; rbp = segmento
+
+; DEFINO LOS CASOS POR NIVEL
+; casos_nivel_0
+test r14, r14
+je .null0
+
+imul rdi, r14, CASO_SIZE ; contador0 * sizeof(caso_t)
+call malloc
+mov [rbp + casos_nivel_0], rax
+
+.null0:
+mov [rbp + casos_nivel_0], 0
+
+; casos_nivel_1
+test r15, r15
+je .null1
+
+imul rdi, r15, CASO_SIZE ; contador1 * sizeof(caso_t)
+call malloc
+mov [rbp + casos_nivel_1], rax
+
+.null1:
+mov [rbp + casos_nivel_1], 0
+
+; casos_nivel_2
+test rbx, rbx
+je .null2
+
+imul rdi, rbx, CASO_SIZE ; contador2 * sizeof(caso_t)
+call malloc
+mov [rbp + casos_nivel_2], rax
+
+.null2:
+mov [rbp + casos_nivel_2], 0
+
+
+xor r8, r8 ; i = 0
+xor r9, r9 ; t0 = 0
+xor r10, r10 ; t1 = 0
+xor r11, r11 ; t2 = 0
+.ciclo:
+    cmp r8, r8
+    jae .fin
+
+    mov rdx, [r12 + r8 * CASO_SIZE + CASO_USUARIO_OFFSET] ; rdx = caso.usuario
+    mov ecx, dword [rdx + USUARIO_NIVEL_OFFSET] ; exc = caso.usuario->nivel
+
+    cmp ecx, 0 ; caso.usuario->nivel == 0
+    je .caso0
+
+    .caso0:
+    inc r9
+    mov [rbp + casos_nivel_0 + r9], 
+
+    jne .ciclo ; si no son el mismo nivel seguimos iterando
+
+    inc rbx ; contador += 1
+    jmp .ciclo
+
+.fin:
+mov rax, rbp
+
+add rsp, 8
+pop rbx
+pop r15
+pop r14
+pop r13
+pop r12
+pop rbp
 ret
--- Guias/Guía de ASM/src/7 - Ejercicios integradores/ej3/ej3a/ej3a.c
+++ Guias/Guía de ASM/src/7 - Ejercicios integradores/ej3/ej3a/ej3a.c
@@ -1,12 +1,43 @@
 #include "../ejs.h"
 
 // Función auxiliar para contar casos por nivel
-void contar_casos_por_nivel(caso_t* arreglo_casos, int largo, int* contadores) {
-}
+int contar_casos_por_nivel(caso_t* arreglo_casos, int largo, int nivel) {
 
+    int contador = 0;
+
+    for (int i = 0; i < largo; i++) {
+        caso_t caso = arreglo_casos[i];
+
+        if (caso.usuario->nivel == nivel)
+            contador += 1;
+        
+    }
+    return contador;
+}
 
 segmentacion_t* segmentar_casos(caso_t* arreglo_casos, int largo) {
+
+    int contador0 = contar_casos_por_nivel(arreglo_casos, largo, 0);
+    int contador1 = contar_casos_por_nivel(arreglo_casos, largo, 1);
+    int contador2 = contar_casos_por_nivel(arreglo_casos, largo, 2);
+
+    segmentacion_t* segmento = (segmentacion_t*) malloc(sizeof(segmentacion_t));
+
+    segmento->casos_nivel_0 = contador0 > 0 ? (caso_t*) malloc(contador0 * sizeof(caso_t)) : NULL;
+    segmento->casos_nivel_1 = contador1 > 0 ? (caso_t*) malloc(contador1 * sizeof(caso_t)) : NULL;
+    segmento->casos_nivel_2 = contador2 > 0 ? (caso_t*) malloc(contador2 * sizeof(caso_t)) : NULL;
+
+    int t0 = 0, t1 = 0, t2 = 0;
+
+    for (int i = 0; i < largo; i++) {
+        caso_t caso = arreglo_casos[i];
+
+        if (caso.usuario->nivel == 0)
+            segmento->casos_nivel_0[t0++] = caso;
+        else if (caso.usuario->nivel == 1)
+            segmento->casos_nivel_1[t1++] = caso;
+        else
+            segmento->casos_nivel_2[t2++] = caso;
+    }
+    return segmento;
 }
-
-
-
